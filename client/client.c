37e16ecf4f7c6f07266846e07a95f27cf0bd17fd
Balcus <bbalcus04@gmail.com>
2026-04-21 21:17:04 +0300

    Added the option to send files to the server

--- client/client.c
+++ client/client.c
@@ -1,33 +1,147 @@
 #include "client.h"
-#include "soapH.h"
 #include "service.nsmap"
+#include "soapH.h"
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <libgen.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 #define SOAP_ENDPOINT "http://localhost:8080"
+#define FILE_SERVER_PORT 8081
+#define BUFFER_SIZE 4096
 
-void client_call_hello(const char *name)
-{
-    struct soap soap;
-    soap_init(&soap);
+void client_call_hello(const char *name) {
+  struct soap soap;
+  soap_init(&soap);
 
-    char *result = NULL;
+  char *result = NULL;
 
-    if (soap_call_ns__hello(
-            &soap,
-            SOAP_ENDPOINT,
-            NULL,
-            (char*)name,
-            &result) == SOAP_OK)
-    {
-        printf("[CLIENT] Server response: %s\n", result);
+  if (soap_call_ns__hello(&soap, SOAP_ENDPOINT, NULL, (char *)name, &result) ==
+      SOAP_OK) {
+    printf("[CLIENT] Server response: %s\n", result);
+  } else {
+    soap_print_fault(&soap, stderr);
+  }
+
+  soap_destroy(&soap);
+  soap_end(&soap);
+  soap_done(&soap);
+}
+
+/*
+ * Trimite fisiere la server prin TCP pe portul FILE_SERVER_PORT.
+ * Protocol:
+ *   Client -> Server:
+ *     - uint32_t num_files
+ *     - pentru fiecare fisier:
+ *         - uint32_t filename_len
+ *         - char[filename_len] filename (doar numele, fara cale)
+ *         - uint32_t file_size
+ *         - char[file_size] file_content
+ *   Server -> Client (echo):
+ *     - acelasi format
+ */
+void client_send_files(const char **filepaths, int count) {
+  /* Creare socket TCP */
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) {
+    perror("[CLIENT] socket");
+    return;
+  }
+
+  struct sockaddr_in serv_addr;
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(FILE_SERVER_PORT);
+  inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+
+  if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    perror("[CLIENT] connect");
+    close(sockfd);
+    return;
+  }
+
+  printf("[CLIENT] Conectat la server pe portul %d\n", FILE_SERVER_PORT);
+
+  /* Trimite numarul de fisiere */
+  uint32_t num_files = (uint32_t)count;
+  write(sockfd, &num_files, sizeof(num_files));
+
+  /* Trimite fiecare fisier */
+  for (int i = 0; i < count; i++) {
+    /* Deschide fisierul cu apel de sistem */
+    int fd = open(filepaths[i], O_RDONLY);
+    if (fd < 0) {
+      fprintf(stderr, "[CLIENT] Nu pot deschide fisierul: %s\n", filepaths[i]);
+      close(sockfd);
+      return;
     }
-    else
-    {
-        soap_print_fault(&soap, stderr);
+
+    /* Determina dimensiunea fisierului */
+    off_t file_size = lseek(fd, 0, SEEK_END);
+    lseek(fd, 0, SEEK_SET);
+
+    /* Extrage doar numele fisierului */
+    char path_copy[BUFFER_SIZE];
+    strncpy(path_copy, filepaths[i], sizeof(path_copy) - 1);
+    path_copy[sizeof(path_copy) - 1] = '\0';
+    char *fname = basename(path_copy);
+    uint32_t fname_len = (uint32_t)strlen(fname);
+
+    /* Trimite: filename_len, filename, file_size, file_content */
+    write(sockfd, &fname_len, sizeof(fname_len));
+    write(sockfd, fname, fname_len);
+
+    uint32_t fsize = (uint32_t)file_size;
+    write(sockfd, &fsize, sizeof(fsize));
+
+    /* Citeste si trimite continutul fisierului */
+    char buf[BUFFER_SIZE];
+    ssize_t bytes_read;
+    while ((bytes_read = read(fd, buf, sizeof(buf))) > 0) {
+      write(sockfd, buf, bytes_read);
     }
 
-    soap_destroy(&soap);
-    soap_end(&soap);
-    soap_done(&soap);
+    close(fd);
+    printf("[CLIENT] Trimis: %s (%u bytes)\n", fname, fsize);
+  }
+
+  /* Primeste fisierele inapoi de la server (echo) */
+  printf("[CLIENT] Astept raspuns de la server...\n");
+
+  uint32_t recv_count = 0;
+  read(sockfd, &recv_count, sizeof(recv_count));
+
+  for (uint32_t i = 0; i < recv_count; i++) {
+    uint32_t fname_len = 0;
+    read(sockfd, &fname_len, sizeof(fname_len));
+
+    char fname[BUFFER_SIZE];
+    read(sockfd, fname, fname_len);
+    fname[fname_len] = '\0';
+
+    uint32_t fsize = 0;
+    read(sockfd, &fsize, sizeof(fsize));
+
+    /* Citeste continutul primit si il afiseaza ca confirmare */
+    uint32_t remaining = fsize;
+    char buf[BUFFER_SIZE];
+    printf("[CLIENT] Primit inapoi: %s (%u bytes)\n", fname, fsize);
+
+    while (remaining > 0) {
+      ssize_t to_read = remaining < sizeof(buf) ? remaining : sizeof(buf);
+      ssize_t n = read(sockfd, buf, to_read);
+      if (n <= 0)
+        break;
+      remaining -= n;
+    }
+  }
+
+  close(sockfd);
+  printf("[CLIENT] Transfer complet.\n");
 }
--- server/server.c
+++ server/server.c
@@ -1,120 +1,258 @@
 #include "server.h"
-#include "soapH.h"
 #include "service.nsmap"
+#include "soapH.h"
 #include "soapStub.h"
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 1024
 
 // file descriptor folosit pentru logging
 int log_fd = -1;
 
-int ns__hello(struct soap *soap, char *name, char **result)
-{
-    char buf[BUFFER_SIZE];
-    snprintf(buf, sizeof(buf), "ns__hello service called");
-    logger_log(buf);
+/* Forward declaration */
+static void *start_file_server_thread(void *arg);
 
-    *result = (char*)soap_malloc(soap, BUFFER_SIZE);
-    snprintf(*result, BUFFER_SIZE, "Hello %s", name);
-    return SOAP_OK;
+int ns__hello(struct soap *soap, char *name, char **result) {
+  char buf[BUFFER_SIZE];
+  snprintf(buf, sizeof(buf), "ns__hello serviciu apelat");
+  logger_log(buf);
+
+  *result = (char *)soap_malloc(soap, BUFFER_SIZE);
+  snprintf(*result, BUFFER_SIZE, "Hello %s", name);
+  return SOAP_OK;
 }
 
 // TODO:
-int ns__register(struct soap *soap, char* username, char *password, int* result) {
-    char buf[BUFFER_SIZE];
-    snprintf(buf, sizeof(buf), "Registered user: %s", username);
-    logger_log(buf);
+int ns__register(struct soap *soap, char *username, char *password,
+                 int *result) {
+  char buf[BUFFER_SIZE];
+  snprintf(buf, sizeof(buf), "User inregistrat: %s", username);
+  logger_log(buf);
 
-    *result = 0;
-    return SOAP_OK;
+  *result = 0;
+  return SOAP_OK;
 }
 
 // TODO:
 int ns__login(struct soap *soap, char *username, char *password, char **token) {
-    char buf[BUFFER_SIZE];
-    snprintf(buf, sizeof(buf), "Logged in user: %s", username);
-    logger_log(buf);
+  char buf[BUFFER_SIZE];
+  snprintf(buf, sizeof(buf), "User logat: %s", username);
+  logger_log(buf);
 
-    *token = soap_malloc(soap, BUFFER_SIZE);
-    strcpy(*token, "token");
-    return SOAP_OK;
+  *token = soap_malloc(soap, BUFFER_SIZE);
+  strcpy(*token, "token");
+  return SOAP_OK;
 }
 
 void logger_init() {
-    mkdir("logs", 0755);
+  mkdir("logs", 0755);
 
-    int pipe_fds[2];
-    if (pipe(pipe_fds) < 0) return;
+  int pipe_fds[2];
+  if (pipe(pipe_fds) < 0)
+    return;
 
-    if (fork() == 0) {
-        close(pipe_fds[1]);
+  if (fork() == 0) {
+    close(pipe_fds[1]);
 
-        char filename[BUFFER_SIZE];
-        time_t now = time(NULL);
-        struct tm *tm_info = localtime(&now);
-        
-        char time_str[64];
-        strftime(time_str, sizeof(time_str), "%Y-%m-%d_%H-%M-%S.log", tm_info);
-        
-        snprintf(filename, sizeof(filename), "logs/%s", time_str);
+    char filename[BUFFER_SIZE];
+    time_t now = time(NULL);
+    struct tm *tm_info = localtime(&now);
 
-        FILE *f = fopen(filename, "w");
-        if (!f) exit(1);
+    char time_str[64];
+    strftime(time_str, sizeof(time_str), "%Y-%m-%d_%H-%M-%S.log", tm_info);
 
-        char buffer[BUFFER_SIZE];
-        ssize_t n;
-        while ((n = read(pipe_fds[0], buffer, sizeof(buffer) - 1)) > 0) {
-            buffer[n] = '\0';
-            fprintf(f, "%s\n", buffer);
-            fflush(f);
-        }
+    snprintf(filename, sizeof(filename), "logs/%s", time_str);
 
-        fclose(f);
-        close(pipe_fds[0]);
-        exit(0); 
+    FILE *f = fopen(filename, "w");
+    if (!f)
+      exit(1);
+
+    char buffer[BUFFER_SIZE];
+    ssize_t n;
+    while ((n = read(pipe_fds[0], buffer, sizeof(buffer) - 1)) > 0) {
+      buffer[n] = '\0';
+      fprintf(f, "%s\n", buffer);
+      fflush(f);
     }
-    
+
+    fclose(f);
     close(pipe_fds[0]);
-    log_fd = pipe_fds[1];
+    exit(0);
+  }
+
+  close(pipe_fds[0]);
+  log_fd = pipe_fds[1];
 }
 
 void logger_log(const char *msg) {
-    if (log_fd < 0) return;
+  if (log_fd < 0)
+    return;
 
-    char timestamp[BUFFER_SIZE];
-    time_t now = time(NULL);
-    struct tm *tm_info = localtime(&now);
-    strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", tm_info);
+  char timestamp[BUFFER_SIZE];
+  time_t now = time(NULL);
+  struct tm *tm_info = localtime(&now);
+  strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", tm_info);
 
-    char final_buf[1024];
-    int len = snprintf(final_buf, sizeof(final_buf), "[%s] %s", timestamp, msg);
-    
-    write(log_fd, final_buf, len);
+  char final_buf[1024];
+  int len = snprintf(final_buf, sizeof(final_buf), "[%s] %s\n", timestamp, msg);
+
+  write(log_fd, final_buf, len);
 }
 
 void start_server(int port) {
-    struct soap soap;
-    soap_init(&soap);
+  struct soap soap;
+  soap_init(&soap);
 
-    logger_init();
+  logger_init();
 
-    if (soap_bind(&soap, NULL, port, 100) < 0) {
-        soap_print_fault(&soap, stderr);
-        return;
+  /* Porneste serverul TCP pentru fisiere intr-un thread separat */
+  pthread_t file_thread;
+  int *file_port = malloc(sizeof(int));
+  *file_port = port + 1; /* portul pentru fisiere = SOAP port + 1 */
+  pthread_create(&file_thread, NULL, start_file_server_thread, file_port);
+
+  if (soap_bind(&soap, NULL, port, 100) < 0) {
+    soap_print_fault(&soap, stderr);
+    return;
+  }
+
+  printf("Server-ul ruleaza pe port-ul %d...\n", port);
+  printf("Server de fisiere ruleaza pe port-ul %d...\n", port + 1);
+
+  logger_log("Server initializat.");
+
+  while (1) {
+    if (soap_accept(&soap) < 0)
+      break;
+    soap_serve(&soap);
+    soap_end(&soap);
+  }
+
+  close(log_fd);
+  soap_done(&soap);
+}
+
+/*
+ * Functie wrapper pentru thread - porneste serverul de fisiere TCP.
+ */
+static void *start_file_server_thread(void *arg) {
+  int port = *(int *)arg;
+  free(arg);
+  start_file_server(port);
+  return NULL;
+}
+
+/*
+ * Server TCP dedicat transferului de fisiere.
+ * Protocol:
+ *   - Primeste: num_files, apoi pentru fiecare: fname_len, fname, fsize,
+ * content
+ *   - Trimite inapoi (echo): acelasi format
+ */
+void start_file_server(int port) {
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd < 0) {
+    perror("[FILE SERVER] socket");
+    return;
+  }
+
+  int opt = 1;
+  setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+  struct sockaddr_in addr;
+  memset(&addr, 0, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = INADDR_ANY;
+  addr.sin_port = htons(port);
+
+  if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+    perror("[FILE SERVER] bind");
+    close(server_fd);
+    return;
+  }
+
+  if (listen(server_fd, 10) < 0) {
+    perror("[FILE SERVER] listen");
+    close(server_fd);
+    return;
+  }
+
+  while (1) {
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    int client_fd =
+        accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+    if (client_fd < 0) {
+      perror("[FILE SERVER] accept");
+      continue;
     }
 
-    printf("Running server on port %d...\n", port);
-    logger_log("Server initialized.");
+    logger_log("Conexiune pentru file transfer acceptata.");
 
-    while (1) {
-        if (soap_accept(&soap) < 0) break;
-        soap_serve(&soap);
-        soap_end(&soap);
+    /* Citeste numarul de fisiere */
+    uint32_t num_files = 0;
+    read(client_fd, &num_files, sizeof(num_files));
+
+    /* Structura temporara pentru stocarea fisierelor primite */
+    typedef struct {
+      char filename[1024];
+      uint32_t fname_len;
+      uint32_t size;
+      char *data;
+    } FileEntry;
+
+    FileEntry *entries = malloc(num_files * sizeof(FileEntry));
+
+    for (uint32_t i = 0; i < num_files; i++) {
+      /* Citeste filename_len + filename */
+      read(client_fd, &entries[i].fname_len, sizeof(uint32_t));
+      read(client_fd, entries[i].filename, entries[i].fname_len);
+      entries[i].filename[entries[i].fname_len] = '\0';
+
+      /* Citeste dimensiunea fisierului */
+      read(client_fd, &entries[i].size, sizeof(uint32_t));
+
+      /* Citeste continutul fisierului */
+      entries[i].data = malloc(entries[i].size);
+      uint32_t remaining = entries[i].size;
+      uint32_t offset = 0;
+      while (remaining > 0) {
+        ssize_t n = read(client_fd, entries[i].data + offset, remaining);
+        if (n <= 0)
+          break;
+        offset += n;
+        remaining -= n;
+      }
+
+      char log_buf[1024];
+      snprintf(log_buf, sizeof(log_buf), "Fisier primit: %s (%u bytes)",
+               entries[i].filename, entries[i].size);
+      logger_log(log_buf);
     }
 
-    close(log_fd);
-    soap_done(&soap);
+    /* Trimite fisierele inapoi (echo) */
+    write(client_fd, &num_files, sizeof(num_files));
+
+    for (uint32_t i = 0; i < num_files; i++) {
+      write(client_fd, &entries[i].fname_len, sizeof(uint32_t));
+      write(client_fd, entries[i].filename, entries[i].fname_len);
+      write(client_fd, &entries[i].size, sizeof(uint32_t));
+      write(client_fd, entries[i].data, entries[i].size);
+      free(entries[i].data);
+    }
+
+    free(entries);
+    close(client_fd);
+    logger_log("Transfer de fisiere complet.");
+  }
+
+  close(server_fd);
 }
